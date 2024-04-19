640460fc83960da31e09cc277e5a62a25c6b286c
Sergio Gurillo <sergio.gurillo11@gmail.com>
2024-04-19 17:41:07 +0200

    start voice assistant on open app

--- isi-wear-os/app/src/main/java/com/guuri11/isi/MainActivity.java
+++ isi-wear-os/app/src/main/java/com/guuri11/isi/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
 
         textToSpeech = new TextToSpeech(this, this);
         isiMessage = findViewById(R.id.text);
+        startVoiceRecognition();
     }
 
     @SuppressLint("SetTextI18n")
@@ -148,6 +149,12 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
      * Called when we click on the Lottie
      */
     public void startVoiceRecognition(View view) {
+        textToSpeech.stop();
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+    }
+    public void startVoiceRecognition() {
         textToSpeech.stop();
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
