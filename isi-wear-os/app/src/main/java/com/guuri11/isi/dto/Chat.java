cbed64c2f48e3ea1b3a9b1accd1b53d9dcd4d5a9
Sergio Gurillo <sergio.gurillo11@gmail.com>
2024-08-03 20:57:46 +0200

    update kotlin app name

--- isi-wear-os/app/src/main/java/com/guuri11/isi/dto/Chat.java
+++
@@ -1,9 +0,0 @@
-package com.guuri11.isi.dto;
-
-public class Chat {
-    private String id;
-
-    public String getId() {
-        return id;
-    }
-}
--- isi-wear-os/app/src/main/java/com/guuri11/isi/persistance/Emoji.java
+++
@@ -1,7 +0,0 @@
-package com.guuri11.isi.persistance;
-
-public class Emoji {
-    public static final String SALUTE_EMOJI = "\uD83D\uDC4B";
-    public static final String THINKING_EMOJI = "\uD83D\uDCAD";
-    public static final String MIC_EMOJI = "\uD83C\uDF99";
-}
